cbac784ccd1297cd8b41dbc8b890082e717e70af
Eric <eric.schubert.3d@gmail.com>
2026-03-13 06:42:30 +0100

    Added problem 7, fixed a issue wich problem 3

--- 0003/0003.py
+++ 0003/0003.py
@@ -12,7 +12,7 @@ primes : [int] = []
 def is_prime(num: int) -> bool:
     if num <= 1:
         return False
-    for i in range(2, math.ceil(math.sqrt(num))):
+    for i in range(2, math.isqrt(num) + 1):
         if num % i == 0:
             return False
     return True
---
+++ 0007/0007.py
@@ -0,0 +1,22 @@
+import math
+
+TARGET = 6
+
+primes : [int] = []
+
+def is_prime(num: int) -> bool:
+    if num <= 1:
+        return False
+    for i in range(2, math.isqrt(num) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+if __name__ == '__main__':
+    i : int = 0
+    while len(primes) < TARGET:
+        if is_prime(i):
+            primes.append(i)
+        i += 1
+    print(primes)
+    print("The %d. prime number is %d" %(TARGET, primes[-1]))
