50e3b412d9eb36d7f877fbcc22c68efed66fb300
Eric <eric.schubert.3d@gmail.com>
2026-03-13 14:08:06 +0100

    Trying more optimization for problem 12

--- 0012/0012.py
+++ 0012/0012.py
@@ -3,6 +3,9 @@ TARGET = 500
 tri_num : int
 i = 2
 calculating = True
+longest = 0
+number = 0
+next = 0
 
 def get_divisors(num : int) -> [int]:
     divisors = []
@@ -13,11 +16,14 @@ def get_divisors(num : int) -> [int]:
     return divisors
 
 while calculating:
-    number= sum(range(1, i))
+    next +=1
+    number += next
     divisors = get_divisors(number)
     if len(divisors) > TARGET:
         tri_num = number
         calculating = False
+    if len(divisors) > longest:
+        longest = len(divisors)
     i += 1
 
 print("The first triangle number with more then %d divisors is: %d"%(TARGET, number))
