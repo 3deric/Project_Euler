5e4090fab7e4610a54504bb3cf98a22549481a5f
Eric <eric.schubert.3d@gmail.com>
2026-03-15 17:54:06 +0100

    Fixed solution for problem 4

--- 0004/0004.py
+++ 0004/0004.py
@@ -1,4 +1,4 @@
-palindrome : tuple()
+palindrome : tuple() = (0,0,0)
 
 # convert the number to a string and invert the string
 # compare if both strings are the same
@@ -6,11 +6,12 @@ def is_palindrome(num) -> bool:
     return str(num) == str(num)[::-1]
 
 if __name__ == '__main__':
-    for i in range(100, 1000):
-        for j in range(100,1000):
+    for i in range(1, 1000):
+        for j in range(1,1000):
             temp = i * j
             if (is_palindrome(temp)):
-                palindrome = (i, j, temp)
+                if temp > palindrome[2]:
+                    palindrome = (i, j, temp)
 
 print("The largest palindrome of two 3-digit numbers is: %d." %(palindrome[2]))
 print("The number is the product of %d and %d." %(palindrome[0], palindrome[1]))
