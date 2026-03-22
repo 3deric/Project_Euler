502a1b9659944ed1c76388a9276bea9e7df69112
Eric <eric.schubert.3d@gmail.com>
2026-03-22 18:43:11 +0100

    Got closer to a working solution, only works with numbers smaller then 1/10

--- 0026/0026.py
+++ 0026/0026.py
@@ -1,48 +1,72 @@
-TARGET = 10
-MAX_DECIMALS = 10
+import time
+
+TARGET = 1000
+MAX_DECIMALS = 1000
 
 # calculate the decimals by performing a simple manual division
 # check for the remainder of the first calculation, save it!
 # if the remainder appears again, break from the loop
 # the result ist the cycle
 
-# print(10 // 4)
-# print((10 // 4 * 4))
-# print(10 - 8)
-# print(2 * 10 // 4)
-
-def calculate_decimals(div : int) -> list[int]:
+def calculate_decimals(div : int) -> tuple():
     num = 1
     result = []
-    first_remainder = -1
-    remainders = []
-
-    while len(result) < MAX_DECIMALS:
-        # calculate the remainder of the division
-        remainder = num * 10 % div
-
+    decimals_cycle = []
+    #while len(result) < MAX_DECIMALS:
+    while True:
         # calculate next decimal
         next = num * 10 // div
-        result.append(next)
 
+        result.append(next)
         # calculate next base number
         num =  num * 10 - (next * div)
-        if len(remainders) > 0:
-            pass
-        remainders.append(remainder)
+        cycle = get_reciprocal_cycles(result)
+        if cycle:
+            decimals_cycle = cycle
+            break
+    return decimals_cycle
 
-    return result
+
+def get_reciprocal_cycles(decimals : list[int]) -> list[int]:
+    cycle = []
+
+    if len(decimals) < 2:
+        return
+    if sum(decimals)  == 0:
+        return [0]
+    while decimals[0] == 0:
+        del decimals[0]
+    for i in range(2, len(decimals) + 1, 2):
+        test_decimals = decimals[-i:]
+        h1 = test_decimals[len(test_decimals)//2:]
+        h2 = test_decimals[:len(test_decimals) // 2]
+        #print(h1,h2)
+        if h1 == h2:
+            cycle = h1
+            break
+    return cycle
+
+start_time = time.time()
+
+longest : tuple = (0,[])
+
+for i in range(2, TARGET):
+    current = calculate_decimals(i)
+    print(i, current)
+    #print(current)
+    #if current[0] == True and len(current[1]) > len(longest[1]):
+    #    longest = (i, current[1])
+
+end_time = time.time()
+#print(longest)
+#print("Time Taken:", end_time - start_time)
 
 # 10 : 6 = 16      # remainder 4 on first calculation
 #  6                # remainder 4 on second calculation too
 #  40               # remainder defines the start and end of the looping area
 
 # 10 : 4 = 25
-#  8
+#  8    decimals_h2 = decimals[half:]
 #  20
 #  20
 #   0
-#for i in range(2, TARGET):
-#    print(i, calculate_decimals(i))
-
-print(calculate_decimals(7))
